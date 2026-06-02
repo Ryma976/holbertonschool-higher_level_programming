8bc3c1c41887a321d2c7188eda11f4d347bb706b
reem <ryma6110@gmail.com>
2026-06-02 13:02:37 +0000

    Fix 8-concat_edges.py by assigning sliced string to str on line 4

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-#
-#
-print(f"{str[38:67]}{str[106:110]} {str[:6]}")
+str = str[38:67] + str[106:110] + " " + str[:6]
+print(str)
