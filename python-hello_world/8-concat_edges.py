130cce386d71d1c6419b5973558d38daefe427e5
reem <ryma6110@gmail.com>
2026-06-02 13:05:22 +0000

    Perfect precision for 8-concat_edges.py with length 40

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[38:67] + str[106:112] + str[:6]
+str = str[38:66] + str[105:111] + str[:6]
 print(str)
