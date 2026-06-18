6adc831bc2668e018861670f49ffebfa476f5b63
ryma976 <ryma976@example.com>
2026-06-18 14:11:39 +0000

    Fix PEP8 line length errors for task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Sets the private width attribute with integer and sign validation."""
+        """Sets width with integer and sign validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -41,7 +41,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Sets the private height attribute with integer and sign validation."""
+        """Sets height with integer and sign validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
