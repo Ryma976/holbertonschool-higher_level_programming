8da033aa9b2ed62029d71a2fd0c1ec3ab7b6516f
reem <ryma6110@gmail.com>
2026-06-24 17:27:38 +0000

    Standard clearance of task 7 files

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,21 +10,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        To satisfy the checker validation:
-        integer_validator()
-        integer_validator("age")
-        integer_validator("age", 1)
-        integer_validator("age", 0)
-        integer_validator("age", -4)
-        integer_validator("age", "4")
-        integer_validator("age", (4,))
-        integer_validator("age", [3])
-        integer_validator("age", True)
-        integer_validator("age", {3, 4})
-        integer_validator("age", None)
-        """
+        """Validate a parameter as an integer."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
