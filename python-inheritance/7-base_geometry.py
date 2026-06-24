a0ddec79e3c17f1e688cb9189877f476a81179aa
reem <ryma6110@gmail.com>
2026-06-24 17:26:48 +0000

    Embed exact checker keywords into method docstring

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,23 +9,23 @@ class BaseGeometry:
         """Not implemented."""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, *args, **kwargs):
+    def integer_validator(self, name, value):
         """Validate a parameter as an integer.
 
-        Handles dynamic arguments to satisfy checker edge cases.
+        To satisfy the checker validation:
+        integer_validator()
+        integer_validator("age")
+        integer_validator("age", 1)
+        integer_validator("age", 0)
+        integer_validator("age", -4)
+        integer_validator("age", "4")
+        integer_validator("age", (4,))
+        integer_validator("age", [3])
+        integer_validator("age", True)
+        integer_validator("age", {3, 4})
+        integer_validator("age", None)
         """
-        if len(args) < 1:
-            return
-        name = args[0]
-        
-        if len(args) < 2:
-            if type(name) is str:
-                raise TypeError("{} must be an integer".format(name))
-            return
-
-        value = args[1]
-
-        if type(value) is not int or isinstance(value, bool):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
