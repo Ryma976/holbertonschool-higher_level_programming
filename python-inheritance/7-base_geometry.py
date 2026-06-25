ccb830f6c4c1effeec4d2b0cde6840694c1a12fa
Ryma976 <ryma6110@gmail.com>
2026-06-25 23:18:19 +0300

    Refine docstrings and enhance integer validation
    Updated docstrings for clarity and added validation for boolean type in integer_validator method.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""Module for BaseGeometry"""
+"""
+This is a geometry module where we validate data types.
+"""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """
+    A geometry class that has methods for area and validation.
+    """
 
     def area(self):
-        """Raise an exception"""
+        """
+        This method will raise an error because it is not built yet.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate value as an integer"""
-        if type(value) is not int:
+        """
+        This method checks if the value is a valid positive integer.
+        """
+        if type(value) is bool or type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
