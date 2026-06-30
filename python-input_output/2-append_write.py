a09b94ed8e5e9d623f907f9089ef59ad855621a0
reem <ryma6110@gmail.com>
2026-06-30 13:57:11 +0000

    Implement task 2: append_write function

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module for append_write function.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a UTF8 text file and returns chars added."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
